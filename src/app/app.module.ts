f8fa1d532c167d5960a8fce0ce40b870a8bbec3e
Jeff Stapleton <jdstapleton@xactware.com>
2019-02-21 18:01:50 -0700

    basic list working

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,13 @@
 import { HttpClientModule } from '@angular/common/http';
-import { CommonModule } from '@angular/common';  
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatNativeDateModule } from '@angular/material';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { AppRoutingModule } from './app-routing.module';
-import { MaterialModule } from './material.module'
+import { MaterialModule } from './material.module';
 import { AppComponent } from './app.component';
 import { CatalogComponent } from './catalog/catalog.component';
 import { ProductComponent } from './components/product/product.component';
@@ -29,6 +30,7 @@ import { AddProductComponent } from './modals/add-product/add-product.component'
     FormsModule,
     HttpClientModule,
     MaterialModule,
+    MatNativeDateModule,
     ReactiveFormsModule
   ],
   entryComponents: [AddProductComponent],
